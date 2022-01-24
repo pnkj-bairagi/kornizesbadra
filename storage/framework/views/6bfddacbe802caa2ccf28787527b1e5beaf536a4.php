bbe5f9601725c578212f3b7a71b869939bfd1618
Pankaj <pankaj.bairagi@mail.valuecoders.com>
2022-01-24 14:14:59 +0530

    fixed cache issue

--- storage/framework/views/6bfddacbe802caa2ccf28787527b1e5beaf536a4.php
+++
@@ -1,7 +0,0 @@
-<!-- FOOTER -->
-<footer class="footer bg-primary text-inverse text-center">
-    <div class="container-fluid">
-        <span class="fs-13 heading-font-family">Copyright @ 2021. <b>Kornizesbadra.com</b> All rights reserved </span>
-    </div>
-    <!-- /.container-fluid -->
-</footer><?php /**PATH /home3/iweblhrx/public_html/kornizesbadra/resources/views/Backend/admin/layout/includes/footer.blade.php ENDPATH**/ ?>
